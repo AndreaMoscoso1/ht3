00c9490473481d9c727e61bd51153381e9240ebd
AndreaMoscoso1 <104097500+AndreaMoscoso1@users.noreply.github.com>
2022-05-09 15:47:44 -0600

    largestlist.py

---
+++ largestlist.py
@@ -0,0 +1,6 @@
+# determinar e imprimir el numero mas grande dentro de una lista desordenada
+elements = (5, 8, 3, 12, 9, 6, 1)
+max = elements(0)
+for i in range (len(elements))
+if (element(i) > max
+   max =elements(i)
